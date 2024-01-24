8741933ed34704128d573a889df1fb5f7dd8cc71
freack21 <fixri2104@gmail.com>
2024-01-24 23:50:40 +0700

    feat[cors]: add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const cors = require("cors");
 
 const PORT = 4063;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
