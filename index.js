7bd3458e3f536f9b9046f20770d2dd093b0be250
freack21 <fixri2104@gmail.com>
2025-08-07 14:00:50 +0700

    make for all api

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.all("/", (req, res) => {
-    res.status(200).json({
-        msg: "it's running!!",
-    });
+  res.status(200).json({
+    msg: "it's running!!",
+  });
 });
 
-app.get("/wa-api", async (req, res) => {
-    req = req.query;
-    const response = await axios.get(req.url, {
-        params: req,
-    });
+app.get("/api", async (req, res) => {
+  req = req.query;
+  const response = await axios.get(req.url, {
+    params: req,
+  });
 
-    res.json(response.data);
+  res.json(response.data);
 });
 
-app.post("/wa-api", async (req, res) => {
-    req = req.body;
-    const response = await axios.post(req.url, req);
+app.post("/api", async (req, res) => {
+  req = req.body;
+  const response = await axios.post(req.url, req);
 
-    res.json(response.data);
+  res.json(response.data);
 });
 
 app.listen(PORT, () => console.log("running at http://localhost:" + PORT));
